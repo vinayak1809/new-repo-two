bb4d92ebb4fc9a02fe32d4762cababf4a0134e61
vinayak1809 <vinayakrshinde18@gmail.com>
2022-07-16 20:09:14 +0530

    task5 commit

---
+++ task5/task5.js
@@ -0,0 +1,5 @@
+var new_ele_class = document.getElementsByClassName('new-element');
+new_ele_class[0].style.fontWeight = 'bold';
+
+var new_ele_tag = document.getElementsByTagName('li')
+new_ele_tag[4].style.backgroundColor = 'blue';
