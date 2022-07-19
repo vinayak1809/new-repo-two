3be12ea68062054ea0b0ce3d79b344bed1fd6980
vinayak1809 <vinayakrshinde18@gmail.com>
2022-07-20 01:58:03 +0530

    task12 commit

---
+++ task12/task12.js
@@ -0,0 +1,51 @@
+
+
+function myfun(event) {
+   
+
+    event.preventDefault();
+    const name = event.target.username.value;
+    const age = event.target.age.value;
+    const mail = event.target.mail.value;
+
+    obj = {
+        name,
+        age,
+        mail
+    }
+
+    localStorage.setItem(obj.mail, 77JSON.stringify(obj));
+    
+    console.log(JSON.parse(localStorage.getItem(mail)))
+   7
+
+}
+ 
+function show_user(localStorage){
+    for (const [key, value] of Object.entries(localStorage)) {
+
+        var usersList = document.getElementById('usersList')
+        var newEle  = document.createElement('li')
+        const ob = JSON.parse(localStorage.getItem(key))
+
+        newEle.appendChild(document.createTextNode(ob.name+ " " + ob.mail))
+        usersList.appendChild(newEle)
+    }
+
+}
+show_user(localStorage)
+
+
+
+
+
+
+
+// function print_Users(user){
+//     const create = document.getElementById('usersList')
+//     const new_ele =  `<li> ${user.name} - ${user.mail}</li>`
+
+//     create.innerHTML = create.innerHTML + new_ele;
+// }
+
+// var store = localStorage.getItem
